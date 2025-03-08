ea26e5cf5a5f7c7652b753fd67cb508285fedcf3
Rafael Vieira <rafael.vieira@vibx.com.br>
2025-03-08 14:48:51 -0300

    Bump tailwind and daisyui

--- client/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    tailwindcss: { config: './src/theme/tailwind.config.ts' },
-    autoprefixer: {},
-  },
-}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
-// import basicSsl from '@vitejs/plugin-basic-ssl'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 })
